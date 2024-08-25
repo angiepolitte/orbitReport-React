dd4dd7903cf03cb34882f02f2e4402fb99de080b
CandyQueen77 <angiepolitte@gmail.com>
2024-08-25 08:40:08 -0500

    3 failed, 6 passed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,58 @@
+// const Table = ({ sat }) => {
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Type of Satellite</th>
+//           <th>Launch Date</th>
+//           <th>Status</th>
+//         </tr>
+//       </thead>
+
+//       <tbody>
+//         const tableData = sat.map((sat) => (
+//         <tr key={sat.id}>
+//           <td>data.name</td>
+//           <td>data.type</td>
+//           <td>data.launchDate</td>
+//           <td>data.operational</td>
+//         </tr>
+//         ))
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// export default Table;
+
 const Table = ({ sat }) => {
   return (
     <table>
       <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
       </thead>
+
       <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+        {sat.map((id, data) => {
+          return (
+            <button onClick={() => Table.data} key={id}>
+              {data}
+            </button>
+          );
+          <tr>
+            <td>data.name</td>
+            <td>data.type</td>
+            <td>data.launchDate</td>
+            <td>data.operational</td>
+          </tr>;
+        })}
+        ;
       </tbody>
     </table>
   );
