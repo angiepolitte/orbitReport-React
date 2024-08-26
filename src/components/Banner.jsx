0ebb9c09bd7d50a9cd3e1e0d1561ca8da4125a60
CandyQueen77 <angiepolitte@gmail.com>
2024-08-26 08:37:50 -0500

    Complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <>
       <h1>Orbit Report</h1>;
-      <p>Click on the buttons to see the satellites in that orbit type</p>;
+      <p>Click on the Buttons to see the Satellites in that Orbit Type</p>;
     </>
   );
 }
