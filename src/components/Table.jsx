384ba255d63bf718e07411dd4f5545d7b441981c
CandyQueen77 <angiepolitte@gmail.com>
2024-08-26 07:34:49 -0500

    Got a table layout!

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,3 @@
-// const Table = ({ sat }) => {
-//   return (
-//     <table>
-//       <thead>
-//         <tr>
-//           <th>Name</th>
-//           <th>Type of Satellite</th>
-//           <th>Launch Date</th>
-//           <th>Status</th>
-//         </tr>
-//       </thead>
-
-//       <tbody>
-//         const tableData = sat.map((sat) => (
-//         <tr key={sat.id}>
-//           <td>data.name</td>
-//           <td>data.type</td>
-//           <td>data.launchDate</td>
-//           <td>data.operational</td>
-//         </tr>
-//         ))
-//       </tbody>
-//     </table>
-//   );
-// };
-
-// export default Table;
-
 const Table = ({ sat }) => {
   return (
     <table>
@@ -39,20 +11,16 @@ const Table = ({ sat }) => {
       </thead>
 
       <tbody>
-        {sat.map((id, data) => {
+        {sat.map((data, id) => {
           return (
-            <button onClick={() => Table.data} key={id}>
-              {data}
-            </button>
+            <tr key={id}>
+              <td>data.name</td>
+              <td>data.type</td>
+              <td>data.launchDate</td>
+              <td>data.operational</td>
+            </tr>
           );
-          <tr>
-            <td>data.name</td>
-            <td>data.type</td>
-            <td>data.launchDate</td>
-            <td>data.operational</td>
-          </tr>;
         })}
-        ;
       </tbody>
     </table>
   );
